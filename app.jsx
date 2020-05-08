8893cc100739bd658ce3f5849c701376ff0120bd
Satoshi Deguchi <deguchik@gmail.com>
2020-05-08 16:49:25 +0900

    update

--- app.jsx
+++ app.jsx
@@ -19,7 +19,7 @@ class App extends Component {
         this.refs.q.focus();
     }
     searchBGG(query) {
-        console.log(query)
+        // console.log(query)
         if (query==='') return this.setState({games: null, message: '', showCreateButton: false});
         this.setState({games: null, message: '検索中…', showCreateButton: false});
         fetch(`https://api.collectio.jp/bggapi/search?query=${encodeURIComponent(query)}`, {
@@ -29,39 +29,45 @@ class App extends Component {
             if (r.status === 'ok') {
                 if (r.data.items.total == 0) {
                     this.setState({message: '見つかりませんでした', showCreateButton: this.refs.q.value});
-                } else if (r.data.items.total == 1) {
-                    const game = r.data.items.item;
-                    // console.log(game.name.value)
-                    this.search(game);
-                    this.setState({games: [game], message: '1件見つかりました'});
                 } else {
-                    const games = r.data.items.item;
-                    games.slice(0, 2).map((game) => {
-                        // console.log(game)
-                        this.search(game);
+                    let games;
+                    if (r.data.items.total == 1) {
+                        games = [r.data.items.item];
+                    } else {
+                        games = r.data.items.item.slice(0, 20);
+                    }
+                    this.setState({games: games, message: r.data.items.total + '件見つかりました'}, () => {
+                        this.search(games[0]);
                     });
-                    setTimeout(() => {
-                        games.slice(2, 10).map((game) => {
-                            // console.log(game)
-                            this.search(game);
-                        });
-                    }, 3000);
-                    this.setState({games: games, message: games.length + '件見つかりました'});
-                }
+                 }
             }
         });
     }
+    changeGames(id, callback) {
+        this.state.games.map((game) => {
+            if (game.id===id) {
+                callback(game);
+            }
+        })
+        this.setState({games: this.state.games});
+    }
     search(game) {
+        // console.log(game)
+        this.changeGames(game.id, (game) => {
+            game.linkTitle = '[検索中...]';
+        });
         const url = `https://db.collectio.jp/wp-json/wp/v2/posts?filter[bgg]=etitle&filter[meta_value]=https://boardgamegeek.com/boardgame/`+game.id;
         fetch(url, {
             mode: 'cors'
         }).then((r) => r.json()).then((r) => {
-            console.log(r)
+            // console.log(r)
             if (r.length > 0) {
                 const dbgame = r[0];
-                console.log(dbgame);
-                this.refs[game.id].href = `https://db.collectio.jp/wp-admin/post.php?post=${dbgame.id}&action=edit`;
-                this.refs[game.id].innerHTML = '[' + dbgame.title.rendered + ']';
+                // console.log(dbgame);
+                this.changeGames(game.id, (game) => {
+                    game.linkTitle = '[' + dbgame.title.rendered + ']';
+                    game.href = `https://db.collectio.jp/wp-admin/post.php?post=${dbgame.id}&action=edit`;
+                });
             } else {
                 fetch(`https://api.collectio.jp/bggapi/thing?id=${game.id}`, {
                     mode: 'cors'
@@ -70,9 +76,12 @@ class App extends Component {
                     if (r.status === 'ok') {
                         const bgggame = r.data.items.item;
                         console.log(bgggame)
-                        const title = encodeURIComponent(bgggame.name.value);
-                        this.refs[game.id].href = `https://db.collectio.jp/wp-admin/post-new.php?post_title=${title}&etitle=${title}&year=${bgggame.yearpublished.value}&bgg=${'https://boardgamegeek.com/boardgame/'+game.id}&playingTime=${bgggame.minplaytime.value}&minPlayers=${bgggame.minplayers.value}&maxPlayers=${bgggame.maxplayers.value}&playAge=${bgggame.minage.value}`;
-                        this.refs[game.id].innerHTML = '[なし→新規追加]';
+                        let title = bgggame.name.length===1 ? bgggame.name.value : bgggame.name[0].value;
+                        title = encodeURIComponent(title);
+                        this.changeGames(game.id, (game) => {
+                            game.linkTitle = '[なし→新規追加]';
+                            game.href = `https://db.collectio.jp/wp-admin/post-new.php?post_title=${title}&etitle=${title}&year=${bgggame.yearpublished.value}&bgg=${'https://boardgamegeek.com/boardgame/'+game.id}&playingTime=${bgggame.minplaytime.value}&minPlayers=${bgggame.minplayers.value}&maxPlayers=${bgggame.maxplayers.value}&playAge=${bgggame.minage.value}`;
+                        });
                     }
                 });
             }
@@ -92,7 +101,11 @@ class App extends Component {
                 {this.state.games && this.state.games.map((game) => (
                     <div className="game" key={game.id}>
                         {game.name.value} - {game.yearpublished ? game.yearpublished.value : ''}
-                        &nbsp;<a ref={game.id} target="_blank">[検索中...]</a>
+                        &nbsp;<a ref={game.id} href={game.href} target="_blank">{game.linkTitle ? (
+                            game.linkTitle
+                        ) : (
+                            <button onClick={this.search.bind(this, game)}>検索</button>
+                        )}</a>
                     </div>
                 ))}
                 {this.state.showCreateButton ? (
